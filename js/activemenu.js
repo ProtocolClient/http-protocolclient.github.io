33a0402183c2c13883a444e84de0f4178924bded
ProtocolClient <43270341+ProtocolClient@users.noreply.github.com>
2018-09-16 14:33:46 +0100

    URLs now absolute (hopefully)

--- js/activemenu.js
+++ js/activemenu.js
@@ -1,10 +1,19 @@
 /// Requires jQuery
 
+function GetFullURL(url){
+    var img = document.createElement('img');
+    img.src = url;
+    url = img.src; 
+    img.src = null; 
+    
+    return url;
+}
+
 $( document ).ready(function() {
     alert("loaded");
     $("nav a").each(function() {
         $(this).removeClass("active-menu");
-        if ($(this).attr("href") == window.location.href)
+        if (GetFullURL($(this).attr("href")) == window.location.href)
             $(this).addClass("active-menu");
         
         alert($(this).attr("href"));        
