01ce8935b65179338bec6cabce916522e90af254
ProtocolClient <43270341+ProtocolClient@users.noreply.github.com>
2018-09-16 14:37:54 +0100

    Update activemenu.js

--- js/activemenu.js
+++ js/activemenu.js
@@ -10,14 +10,9 @@ function GetFullURL(url){
 }
 
 $( document ).ready(function() {
-    alert("loaded");
     $("nav a").each(function() {
         $(this).removeClass("active-menu");
         if (GetFullURL($(this).attr("href")) == window.location.href)
             $(this).addClass("active-menu");
-        
-        alert(GetFullURL($(this).attr("href")));        
-        alert(window.location.href);
-
     });
 });
